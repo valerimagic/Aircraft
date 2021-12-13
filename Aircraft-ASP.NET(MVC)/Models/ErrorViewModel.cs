813a8507b6a0dcb63e61f865fd91743e6959c6c9
Valeri Hristov <valerimagic@gmail.com>
2021-12-13 23:32:43 +0200

    update

---
+++ Aircraft-ASP.NET(MVC)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aircraft_ASP.NET_MVC_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
