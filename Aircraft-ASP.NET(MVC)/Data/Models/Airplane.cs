8ce9dbdfde8fe3c4f4036a652ee7980e96e2759e
Valeri Hristov <valerimagic@gmail.com>
2021-12-13 23:32:17 +0200

    update

---
+++ Aircraft-ASP.NET(MVC)/Data/Models/Airplane.cs
@@ -0,0 +1,15 @@
+﻿namespace Aircraft_ASP.NET_MVC_.Data.Models
+{
+    public class Airplane
+    {
+        public int Id { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int Year { get; set; }
+    }
+}
