024f5f0f444562b2f1c06f379956c6a2618b2012
Valeri Hristov <valerimagic@gmail.com>
2021-12-13 23:31:45 +0200

    update

---
+++ Aircraft-ASP.NET(MVC)/Data/AircraftDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Aircraft_ASP.NET_MVC_.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AircraftDbContext : IdentityDbContext
+    {
+        public AircraftDbContext(DbContextOptions<AircraftDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
