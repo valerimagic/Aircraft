e6287f111d011b5860de93e2708200ce2b908846
Valeri Hristov <valerimagic@gmail.com>
2021-12-15 22:49:43 +0200

    update

--- Aircraft-ASP.NET(MVC)/Data/AircraftDbContext.cs
+++ Aircraft-ASP.NET(MVC)/Data/AircraftDbContext.cs
@@ -1,10 +1,13 @@
-﻿namespace Aircraft_ASP.NET_MVC_.Data
+﻿using Aircraft_ASP.NET_MVC_.Data.Models;
+
+namespace Aircraft_ASP.NET_MVC_.Data
 {
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
 
     public class AircraftDbContext : IdentityDbContext
     {
+        //public DbContext<Airplane> Airplanes { get; set; }
         public AircraftDbContext(DbContextOptions<AircraftDbContext> options)
             : base(options)
         {
--- Aircraft-ASP.NET(MVC)/Data/Models/Airplane.cs
+++ Aircraft-ASP.NET(MVC)/Data/Models/Airplane.cs
@@ -1,12 +1,22 @@
 ﻿namespace Aircraft_ASP.NET_MVC_.Data.Models
 {
+    using System.ComponentModel.DataAnnotations;
+    using static DataConstants;
     public class Airplane
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(AirPlaneBrandMaxLength)]
         public string Brand { get; set; }
 
+        [Required]
+        [MaxLength(AirPlaneModelMaxLength)]
         public string Model { get; set; }
+
+        [Required]
+        [MaxLength(AirPlaneDescriptionMaxLength)]
+        public string Description { get; set; }
 
         public string ImageUrl { get; set; }
 
