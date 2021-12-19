9c1c1f8e0f5cccbfeda9ae91c88588a1011a68ee
Valeri Hristov <valerimagic@gmail.com>
2021-12-19 23:01:44 +0200

    update

--- Aircraft-ASP.NET(MVC)/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ Aircraft-ASP.NET(MVC)/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -9,7 +9,7 @@ using System;
 
 namespace Aircraft_ASP.NET_MVC_.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(AircraftDbContext))]
     [Migration("00000000000000_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
