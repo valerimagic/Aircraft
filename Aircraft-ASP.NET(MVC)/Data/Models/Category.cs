a0d262f4817647128b71391be9d773e43e464029
Valeri Hristov <valerimagic@gmail.com>
2022-01-01 23:31:28 +0200

    update init

--- Aircraft-ASP.NET(MVC)/Data/Models/Category.cs
+++ Aircraft-ASP.NET(MVC)/Data/Models/Category.cs
@@ -5,7 +5,7 @@ namespace Aircraft_ASP.NET_MVC_.Data.Models
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; init; }
         public IEnumerable<Airplane> Airplanes { get; set; } = new List<Airplane>();
     }
 }
