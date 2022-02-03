3c8fba5eb6955d1fad47140dd4fba35334beb32a
Valeri Hristov <valerimagic@gmail.com>
2022-02-03 23:31:43 +0200

    First migration

---
+++ Aircraft-ASP.NET(MVC)/Data/Migrations/20220203170653_03022.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aircraft_ASP.NET_MVC_.Data.Migrations
+{
+    public partial class _03022 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
