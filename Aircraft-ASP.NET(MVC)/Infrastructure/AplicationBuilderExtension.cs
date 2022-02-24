2a3a812589106eb076eb6971b0a2eab36df3308f
Valeri Hristov <valerimagic@gmail.com>
2022-02-24 23:13:03 +0200

    Add method for auto migration

--- Aircraft-ASP.NET(MVC)/Infrastructure/AplicationBuilderExtension.cs
+++ Aircraft-ASP.NET(MVC)/Infrastructure/AplicationBuilderExtension.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Aircraft_ASP.NET_MVC_.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Aircraft_ASP.NET_MVC_.Infrastructure
 {
@@ -10,6 +13,11 @@ namespace Aircraft_ASP.NET_MVC_.Infrastructure
     {
         public static IApplicationBuilder PrepareDatabase(this IApplicationBuilder app) //ext metod koito se griji da podgotvi DB, извикава метод върху интерфейса
         {
+            using var scopedServiced = app.ApplicationServices.CreateScope();
+            var data = scopedServiced.ServiceProvider.GetService<AircraftDbContext>();
+
+            data.Database.Migrate();
+
             return app;
         }
     }
