fda9862b4a4f2d34a2acc1a011f064e3171fe245
Valeri Hristov <valerimagic@gmail.com>
2022-03-02 23:44:21 +0200

    Create "Model Binding"

---
+++ Aircraft-ASP.NET(MVC)/Controllers/AircraftController.cs
@@ -0,0 +1,17 @@
+﻿using Aircraft_ASP.NET_MVC_.Models.Aircrafts;
+
+namespace Aircraft_ASP.NET_MVC_.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AircraftController : Controller
+    {
+        public IActionResult Add() => View();
+
+        [HttpPost]
+        public IActionResult Add(AddAirplaneFormModel plane)
+        {
+            return View();
+        }
+    }
+}
---
+++ Aircraft-ASP.NET(MVC)/Models/Aircrafts/AddAirplaneFormModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Aircraft_ASP.NET_MVC_.Models.Aircrafts
+{
+    public class AddAirplaneFormModel
+    {
+        
+        public string Brand { get; init; }
+        
+        public string Model { get; init; }
+        
+        public string Description { get; init; }
+
+        public string ImageUrl { get; init; }
+
+        public int Year { get; init; }
+
+        public int CategoryId { get; init; }
+    }
+}
